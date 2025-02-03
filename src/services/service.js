94fbe0dc1da45f63a9074c52147902d6d809dd1b
Marcos-Monte <marcos.monte.junior@gmail.com>
2025-02-03 20:53:44 -0300

    fix: erro de impressão / salvamento na versão mobile em produção

--- src/services/service.js
+++ src/services/service.js
@@ -2,25 +2,38 @@
 
 async function imprimir(){
 
-    // Mostra um alerta ao usuário
-    alert('Duas caixas de diálogo de impressão serão abertas. Por favor, salve uma e imprima a outra.');
+    // Verificar se o usuario está em um dispositivo mobile
+    const isMobile = /Android|IPhone|iPad|iPod/i.test(navigator.userAgent);
 
-    // Abre a primeira caixa de impressão
-    window.print();
+    if(isMobile){
 
-    // Após a primeira impressão, abre a segunda caixa de impressão após um pequeno atraso
-    setTimeout(() => {
-        // Abre a segunda caixa de impressão
+        // Abre a primeira caixa de impressão
         window.print();
 
-        // Após a segunda impressão, oculta o conteúdo e recarrega a página
-        setTimeout(() => {
-            
-            window.location.reload();
-        }, 100);
-    }, 500); // Ajuste o tempo se necessário para garantir que o usuário tenha tempo suficiente
+        // Após a 'impressão', oculta o conteúdo e recarrega a página
+        window.location.reload();
 
-    
+    } else {
+
+        // Mostra um alerta ao usuário
+        alert('Duas caixas de diálogo de impressão serão abertas. Por favor, salve uma e imprima a outra.');
+
+        // Abre a primeira caixa de impressão
+        window.print();
+
+        // Após a primeira impressão, abre a segunda caixa de impressão após um pequeno atraso
+        setTimeout(() => {
+            // Abre a segunda caixa de impressão
+            window.print();
+
+            // Após a segunda impressão, oculta o conteúdo e recarrega a página
+            setTimeout(() => {
+                
+                window.location.reload();
+            }, 100);
+        }, 500); // Ajuste o tempo se necessário para garantir que o usuário tenha tempo suficiente
+
+    }
     
 };
 
